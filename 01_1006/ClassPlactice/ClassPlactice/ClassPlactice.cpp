60e8d6a4bb6be8b4f76d94d24ee2e5bf235c2011
MSI\papyl <papylus.0414@iCloud.com>
2020-10-06 11:06:39 +0900

    クラスの作り方の復習プロジェクトの作成

---
+++ 01_1006/ClassPlactice/ClassPlactice/ClassPlactice.cpp
@@ -0,0 +1,37 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+
+// Figiterクラスを作成します
+class Figter
+{
+public:
+
+    int m_hp;
+    int m_str;
+    int m_mag;
+    int m_tec;
+    int m_spd;
+    int m_def;
+    int m_iuc;
+    int m_mdef;
+
+    //コンストラクタ
+    Figter();
+
+    //デストラクタ
+    ~Figter();
+};
+
+Figter::Figter()
+{
+
+}
+
+int main()
+{
+    
+    system("pause");
+    return 0;
+
+}
+
